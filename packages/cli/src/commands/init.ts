452e18dae5bd5badc08546d37dbc8c76afe37a88
dannybreck <142409809+dannybreck@users.noreply.github.com>
2025-05-20 23:42:21 -0400

    chore(init): drop TS type for ESLint sanity

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -2,7 +2,7 @@ import { promises as fs } from 'fs';
 import path from 'path';
 
 export async function run (
-  stack: 'astro' | 'next' = 'astro',
+  stack = 'astro',
   output = 'frontend'
 ) {
   const cfgFile = 'staticpress.config.js';
